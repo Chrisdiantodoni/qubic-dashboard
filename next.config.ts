276ce454d7a4d609ad5dc847a20c1be4328bf75e
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2025-04-26 05:13:52 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   removeConsole: process.env.NODE_ENV === "production" ? false : true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Authorization, Content-Type",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
