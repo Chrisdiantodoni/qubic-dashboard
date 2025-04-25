b7d456f985cb006e92026920f8412649e3c6d3a9
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2025-04-26 05:04:16 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  removeConsole: process.env.NODE_ENV === "production" ? false : true,
+};
 
 export default nextConfig;
